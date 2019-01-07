e8f9fc90ebcf7323fe8b1372ab8143ee1e4ab06c
seonggu.baek <seonggu.baek@nexstreaming.com>
2019-01-07 17:44:54 +0900

    Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-interface Human {
-    name: string;
-    age: number;
-    gender?: string;   
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "taylor",
-    age: 38,
-    gender: "male"
-}
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender?: string;   
+// }
+
+// const person = {
+//     name: "taylor",
+//     age: 38,
+//     gender: "male"
+// }
+
+const person = new Human("taylor", 28, "male");
         
 const sayHi = (person: Human) => {
     return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
